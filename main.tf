68ff4fe6f83f1c52d95af741a75045e188fe1c25
Marius Tolzmann <marius@mineiros.io>
2022-04-17 04:32:23 +0200

    fix: Add missing depends_on in main resource

--- main.tf
+++ main.tf
@@ -4,10 +4,13 @@ resource "google_project" "project" {
   project_id = var.project_id
   name       = var.name != null ? var.name : var.project_id
 
-  org_id              = var.org_id
-  folder_id           = var.folder_id
+  org_id    = var.org_id
+  folder_id = var.folder_id
+
   billing_account     = var.billing_account
   skip_delete         = var.skip_delete
   labels              = var.labels
   auto_create_network = var.auto_create_network
+
+  depends_on = [var.module_depends_on]
 }
